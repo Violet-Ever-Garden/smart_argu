b6e9a28c69b7695fdf71dd9554b2e375a40ed201
haokai <2398894692@qq.com>
2020-08-13 15:51:31 +0800

    增加班级老师接口

---
+++ hzau-sa-backstage/src/main/java/hzau/sa/backstage/service/AsTeacherclassService.java
@@ -0,0 +1,16 @@
+package hzau.sa.backstage.service;
+
+import org.springframework.stereotype.Service;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import hzau.sa.backstage.dao.AsTeacherclassDao;
+import hzau.sa.backstage.entity.AsTeacherclassVO;
+
+/**
+ * as_teacherclass 服务实现类
+ * @author haokai
+ * @date 2020-08-13
+ */
+public interface AsTeacherclassService  {
+
+
+}
