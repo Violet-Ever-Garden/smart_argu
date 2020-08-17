2813be8dcef0775980b7326eb9b0a242c5941704
haolvh <2569551290@qq.com>
2020-08-18 01:15:10 +0800

    add FileWebConfig

---
+++ hzau-sa-msg/src/main/java/hzau/sa/msg/config/FileWebConfig.java
@@ -0,0 +1,40 @@
+package hzau.sa.msg.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+/**
+ * @author LvHao
+ * @Description : 网路路径的映射配置
+ * @date 2020-08-18 1:06
+ */
+@Configuration
+public class FileWebConfig extends WebMvcConfigurationSupport {
+
+    @Value("${file.map}")
+    private String FILE_MAP;
+
+    @Value("${file.upload.path}")
+    private String path;
+
+
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler(FILE_MAP)
+                .addResourceLocations("file:" + path);
+
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+        registry.addResourceHandler("/csrf/**")
+                .addResourceLocations("classpath:/META-INF/resources/csrf/");
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/**");
+
+
+        super.addResourceHandlers(registry);
+    }
+}
