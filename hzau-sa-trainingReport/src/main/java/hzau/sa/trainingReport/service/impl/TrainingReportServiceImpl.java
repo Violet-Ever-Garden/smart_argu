3fbfa4d898f3bfe43a389bab7406cfae510ad303
haolvh <2569551290@qq.com>
2020-08-20 17:06:26 +0800

    commit export trainingReport

--- hzau-sa-trainingReport/src/main/java/hzau/sa/trainingReport/service/impl/TrainingReportServiceImpl.java
+++ hzau-sa-trainingReport/src/main/java/hzau/sa/trainingReport/service/impl/TrainingReportServiceImpl.java
@@ -1,5 +1,7 @@
 package hzau.sa.trainingReport.service.impl;
 
+import hzau.sa.msg.excel.MyExcelUtil;
+import hzau.sa.trainingReport.entity.ExportReport;
 import hzau.sa.trainingReport.entity.TrainingReportVO;
 import hzau.sa.trainingReport.dao.TrainingReportDao;
 import hzau.sa.trainingReport.entity.TrainingReportVO;
@@ -10,6 +12,8 @@ import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.io.File;
+import java.util.List;
 
 /**
  * <p>
@@ -30,7 +34,24 @@ public class TrainingReportServiceImpl extends ServiceImpl<TrainingReportDao, Tr
 
     @Override
     public String excelDir(String cropId, String[] classIds, String teacherId) {
-        System.out.println(excelFilePath);
+
+        File file = new File(excelFilePath);
+        if(!file.exists()){
+            file.mkdirs();
+        }
+
+        try{
+            for(String classId : classIds){
+                List<ExportReport> exportReportList = trainingReportDao.queryExportReportByCropClassTeacherIds(Integer.valueOf(cropId),Integer.valueOf(classId),teacherId);
+                String fileName = trainingReportDao.queryCropNameById(Integer.valueOf(cropId)) + "_" +
+                        trainingReportDao.queryGradeNameByClassId(Integer.valueOf(classId)) + "_" +
+                        trainingReportDao.queryClassNameById(Integer.valueOf(classId));
+                MyExcelUtil.generateExcel(excelFilePath,fileName, ExportReport.class,exportReportList);
+            }
+        }catch (Exception e){
+            log.error(e.toString());
+            throw e;
+        }
         return excelFilePath;
     }
 }
