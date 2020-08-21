84637926b1464130541a0721de2d083d967c62b9
haolvh <2569551290@qq.com>
2020-08-21 14:34:42 +0800

    integration baskstage and trainingReport to starter

--- hzau-sa-stater/src/main/java/hzau/sa/stater/Application.java
+++ hzau-sa-stater/src/main/java/hzau/sa/stater/Application.java
@@ -4,12 +4,14 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@ComponentScan("hzau.sa.*")
-@MapperScan("hzau.sa.*")
-@SpringBootApplication
+@EnableTransactionManagement
+@ComponentScan(basePackages = {"hzau.sa.*"})
+@MapperScan(basePackages = {"hzau.sa.*.dao"})
 @EnableSwagger2
+@SpringBootApplication
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class,args);
--- hzau-sa-trainingReport/src/main/java/hzau/sa/trainingReport/dao/TrainingReportStudentDao.java
+++ hzau-sa-trainingReport/src/main/java/hzau/sa/trainingReport/dao/TrainingReportStudentDao.java
@@ -1,15 +1,15 @@
 package hzau.sa.trainingReport.dao;
 
-import hzau.sa.trainingReport.entity.AsTeacherclassVO;
 import org.apache.ibatis.annotations.Mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import hzau.sa.trainingReport.entity.StudentVO;
 
 /**
- * AsTeacherclass Mapper 接口
+ * Student Mapper 接口
  * @author lvhao
  * @date 2020-08-19
  */
 @Mapper
-public interface AsTeacherclassDao extends BaseMapper<AsTeacherclassVO> {
+public interface TrainingReportStudentDao extends BaseMapper<StudentVO> {
 
 }
