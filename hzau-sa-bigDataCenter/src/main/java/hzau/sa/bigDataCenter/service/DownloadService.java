8a071c56218745a5d42a90f2c3d750238c971609
OswaldZero <wyh1726036835@126.com>
2020-09-11 14:36:44 +0800

    修改实训报告老师评语状态

--- hzau-sa-bigDataCenter/src/main/java/hzau/sa/bigDataCenter/service/DownloadService.java
+++ hzau-sa-bigDataCenter/src/main/java/hzau/sa/bigDataCenter/service/DownloadService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import hzau.sa.bigDataCenter.dao.DownloadDao;
 import hzau.sa.bigDataCenter.entity.DownloadVO;
+import org.springframework.web.multipart.MultipartFile;
 
 /**
  * download 服务实现类
@@ -12,6 +13,8 @@ import hzau.sa.bigDataCenter.entity.DownloadVO;
  * @date 2020-09-06
  */
 public interface DownloadService  {
-    
-
+    public Result uploadFolder(String fileName,String fileIntroduction,MultipartFile[] files);
+    public Result deleteFiles(Integer[] fileIds);
+    public Result page();
+    public Result downloadFile(Integer fileId);
 }
--- hzau-sa-bigDataCenter/src/main/java/hzau/sa/bigDataCenter/service/impl/DownloadServiceImpl.java
+++ hzau-sa-bigDataCenter/src/main/java/hzau/sa/bigDataCenter/service/impl/DownloadServiceImpl.java
@@ -4,7 +4,11 @@ import hzau.sa.bigDataCenter.dao.DownloadDao;
 import hzau.sa.bigDataCenter.entity.DownloadVO;
 import hzau.sa.bigDataCenter.service.DownloadService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import hzau.sa.msg.entity.Result;
+import hzau.sa.msg.util.FileUtil;
+import hzau.sa.msg.util.ResultUtil;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 /**
  * <p>
@@ -16,5 +20,44 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class DownloadServiceImpl extends ServiceImpl<DownloadDao, DownloadVO> implements DownloadService {
+    /**
+     * 文件或者文件夹上传
+     * @param fileName
+     * @param fileIntroduction
+     * @param files
+     * @return
+     */
+    @Override
+    public Result uploadFolder(String fileName, String fileIntroduction, MultipartFile[] files){
+        return ResultUtil.success();
+    }
 
+    /**
+     * 文件删除
+     * @param fileIds
+     * @return
+     */
+    @Override
+    public Result deleteFiles(Integer[] fileIds){
+        return ResultUtil.success();
+    }
+
+    /**
+     * 分页
+     * @return
+     */
+    @Override
+    public Result page(){
+        return ResultUtil.success();
+    }
+
+    /**
+     * 文件下载
+     * @param fileId
+     * @return
+     */
+    @Override
+    public Result downloadFile(Integer fileId){
+        return ResultUtil.success();
+    }
 }
